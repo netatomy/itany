469a9777207d4eb63f59ea827e89411a6de0e4db
qczhang <qczhang@qczhang-PC.mshome.net>
2012-10-31 13:51:17 +0800

    Lesson 1   Section 1: Hello World

---
+++ CoreJava/src/zqc/corejava/lesson01/s01/HelloWorld.java
@@ -0,0 +1,13 @@
+package zqc.corejava.lesson01.s01;
+
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+
+}
