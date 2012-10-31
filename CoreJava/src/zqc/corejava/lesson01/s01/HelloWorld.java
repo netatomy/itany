5d6899d82966d983f13b1dbfc32850c4a02b00ac
qczhang <qiancheng.zhang@qq.com>
2012-10-31 14:16:19 +0800

    make some changes.

--- CoreJava/src/zqc/corejava/lesson01/s01/HelloWorld.java
+++ CoreJava/src/zqc/corejava/lesson01/s01/HelloWorld.java
@@ -3,10 +3,10 @@ package zqc.corejava.lesson01.s01;
 public class HelloWorld {
 
     /**
+     * 文档注释
      * @param args
      */
     public static void main(String[] args) {
-
         System.out.println("Hello World");
     }
 
